973ac0491b63c9ebe56acf51398e9d2a9529b015
Anmol Kagrecha <akagrecha@gmail.com>
2021-11-28 11:44:15 -0800

    bootstrap, fixed calib_err

--- evaluate.py
+++ evaluate.py
@@ -44,14 +44,17 @@ def calib_err(confidence, correct, p='2', beta=100):
 
 def evaluate(net,device,test_dataset,batch_size):
     net.eval()
+    device_loc = "cpu"
+
     test_dataloader = DataLoader(test_dataset,batch_size=batch_size, shuffle=False)
-    t_confidences = torch.Tensor()
-    t_correct = torch.Tensor()
-    p_confidences = torch.Tensor()
-    p_correct = torch.Tensor()
+    t_confidences = torch.Tensor().to(device_loc)
+    t_correct = torch.Tensor().to(device_loc)
+    p_confidences = torch.Tensor().to(device_loc)
+    p_correct = torch.Tensor().to(device_loc)
     vloss = []
     num_total = 0.0
     t_num_correct = 0.0
+    p_num_correct = torch.zeros(net.num_heads).to(device_loc)
     criterion = nn.CrossEntropyLoss()
     threshold=0.9 ###MAKE PARARM
     num_common_corrects = 0.0
@@ -59,63 +62,55 @@ def evaluate(net,device,test_dataset,batch_size):
     num_common_incorrects_high_conf = 0.0
     num_common_corrects_high_conf = 0.0
 
+    
+
     with torch.no_grad():
         for i, (img,lbl,_) in enumerate(tqdm(test_dataloader)):
             img = img.to(device)
             lbl = lbl.to(device)
-            tar_conf, p_confs = net(img.to(device))
+            t_out, p_outs = net(img.to(device))
 
-            loss = criterion(tar_conf,lbl)
-            vloss.append(loss.cpu())
+            loss = criterion(t_out,lbl)
+            vloss.append(loss.to(device_loc))
 
-            t_conf, t_pred = tar_conf.data.max(1)
-            t_conf,t_pred = t_conf.cpu(), t_pred.cpu()
-            lbl = lbl.cpu()
+            t_conf, t_pred = t_out.data.max(1)
+            t_conf, t_pred = t_conf.to(device_loc), t_pred.to(device_loc)
+            lbl = lbl.to(device_loc)
+
             t_num_correct += (t_pred==lbl).double().sum().item()
             num_total += t_pred.size(0)
             t_confidences = torch.cat([t_confidences, t_conf])
             t_correct = torch.cat([t_correct, t_pred.eq(lbl)])
-            # if t_conf.shape[0] > 1:
-            #     t_confidences.extend(t_conf.data.numpy().squeeze().tolist())
-            #     t_correct.extend(t_pred.eq(lbl).numpy().squeeze().tolist())
-            # else:
-            #     t_confidences.append(t_conf.data.numpy().squeeze())
-            #     t_correct.append(t_pred.eq(lbl).numpy().squeeze())
 
             if net.num_heads>0:
-                common_corr = torch.ones(t_conf.shape[0])
-                common_inc = torch.ones(t_conf.shape[0])
-                common_inc_high_conf = torch.zeros(t_conf.shape[0])
-                common_corr_high_conf = torch.zeros(t_conf.shape[0])
+                common_corr = torch.ones(t_conf.shape[0]).to(device_loc)
+                common_inc = torch.ones(t_conf.shape[0]).to(device_loc)
+                common_inc_high_conf = torch.zeros(t_conf.shape[0]).to(device_loc)
+                common_corr_high_conf = torch.zeros(t_conf.shape[0]).to(device_loc)
                 max_confs = torch.zeros(t_conf.shape[0])
-                for ph_conf in p_confs:
-                    p_conf, p_pred = ph_conf.data.max(1)
-                    p_conf,p_pred = p_conf.cpu(), p_pred.cpu()
+                for i,p_out in enumerate(p_outs):
+                    p_conf, p_pred = p_out.data.max(1)
+                    p_conf, p_pred = p_conf.to(device_loc), p_pred.to(device_loc)
+                    p_num_correct[i] += (p_pred==lbl).double().sum().item()
 
                     max_confs = torch.max(max_confs,p_conf)
-                    p_corr = (p_conf==lbl)
-                    p_inc = (p_conf!=lbl)
+                    p_corr = (p_pred==lbl)
+                    p_inc = (p_pred!=lbl)
                     common_corr *= p_corr
                     common_inc *= p_inc #B,
                     
                     high_conf = p_conf>threshold
                     p_inc_high = high_conf*p_inc
                     p_corr_high = high_conf*p_corr
-                    common_inc_high_conf = (common_inc_high_conf+p_inc_high).clamp(0,1)
-                    common_corr_high_conf = (common_corr_high_conf+p_corr_high).clamp(0,1)
+                    common_inc_high_conf = ((common_inc_high_conf+p_inc_high).clamp(0,1))*common_inc
+                    common_corr_high_conf = ((common_corr_high_conf+p_corr_high).clamp(0,1))*common_corr
 
-                    ens_p_conf = max_confs*(common_corr_high_conf-common_inc_high_conf)
-                    ens_pred = (common_corr_high_conf+common_inc_high_conf).clamp(0,1)
+                    ens_p_conf = max_confs*((common_corr_high_conf+common_inc_high_conf).clamp(0,1))
+                    ens_corr = common_corr_high_conf
 
                 p_confidences = torch.cat([p_confidences, ens_p_conf])
-                p_correct = torch.cat([p_correct, ens_pred])
-                # if ens_p_conf.shape[0] > 1:
-                #     p_confidences.extend(ens_p_conf.data.numpy().squeeze().tolist())
-                #     p_correct.extend(ens_pred.data.numpy().squeeze().tolist())
-                # else:
-                #     p_confidences.append(ens_p_conf.data.numpy().squeeze())
-                #     p_correct.append(ens_pred.data.numpy().squeeze())
-
+                p_correct = torch.cat([p_correct, ens_corr])
+                
                 num_common_corrects += common_corr.double().sum().item()
                 num_common_incorrects += common_inc.double().sum().item()
                 num_common_incorrects_high_conf += common_inc_high_conf.double().sum().item()
@@ -125,35 +120,42 @@ def evaluate(net,device,test_dataset,batch_size):
     t_cerr = calib_err(np.array(t_confidences),np.array(t_correct)) 
     p_cerr = calib_err(np.array(p_confidences),np.array(p_correct)) 
     acc = t_num_correct/num_total  
+    acc_pheads = p_num_correct/num_total
 
     com_corr_high = num_common_corrects_high_conf/num_total
     com_corr = num_common_corrects/num_total
     com_inc = num_common_incorrects/num_total
     com_inc_high = num_common_incorrects_high_conf/num_total
     disag = 1.0 - com_corr - com_inc
-    return val_loss, acc, t_cerr, (com_corr_high, com_corr, com_inc, com_inc_high, disag, p_cerr)
+    return val_loss, (acc, acc_pheads), t_cerr, (com_corr_high, com_corr, com_inc, com_inc_high, disag, p_cerr)
 
 if __name__=="__main__":
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print("Device={}".format(device))
-    batch_size = 2
+    batch_size = 4
     num_classes = 62
     num_pseudo_heads = 0
 
-    # net = HydraNet(num_heads=num_pseudo_heads, num_features=1024,
-    #     num_classes=num_classes,pretrained=False)
-    # net = net.to(device) 
-    # load_model(net, "C:\\Users\\akagr\\model_weights\\checkpoints\\baseline\\source_trained_2021-11-23-22-44-52_10.pt")
+    net = HydraNet(num_heads=num_pseudo_heads, num_features=1024,
+        num_classes=num_classes,pretrained=False)
+    net = net.to(device) 
+    load_model(net, "C:\\Users\\akagr\\hypermodel-ss\\model_weights_vm\\baseline\\source_trained_10.pt")
 
-    net = torchvision.models.densenet.densenet121()
+    #net = torchvision.models.densenet.densenet121()
 
-    pretrained_dict = torch.load("pretrained/fmow_seed_0_epoch_best_model.pth")
-    net.load_state_dict(pretrained_dict['algorithm'])
-    net = net.to(device)
+    # pretrained_dict = torch.load("pretrained/fmow_seed_0_epoch_best_model.pth")
+    # net.load_state_dict(pretrained_dict['algorithm'])
+    # net = net.to(device)
     
     dataset = get_dataset(dataset='fmow_mini', download=False)
-    test_dataset = dataset.get_subset('val', 
+    test_dataset = dataset.get_subset('val', frac=1,
         transform=transforms.Compose([transforms.Resize((224,224)),transforms.ToTensor()]))
-    test_loss, test_acc, test_cerr = evaluate(net,device,test_dataset,batch_size)
-    print("Test Loss={}, Test Acc={}, Test Calib Error={}".format(test_loss, test_acc, test_cerr))
+    test_loss, accs, test_cerr, val_pHead_stats = evaluate(net,device,test_dataset,batch_size)
+    com_corr_high, com_corr, com_inc, com_inc_high, disag, p_cerr = val_pHead_stats
+    print("Target Loss={}, Target Acc={}, Target Calib Error={}".format(test_loss, accs[0], test_cerr))
+    print(f"Pseudo head accuracies: {accs[1]}")
+    print("com_corr_high={:.7f}, com_corr={:.7f}, com_inc={:.7f}, com_inc_high={:.7f}, disag={:.7f}, P_Cal Error={:.7f}".format(
+                com_corr_high, com_corr,com_inc,com_inc_high,disag,p_cerr))
+            
+    print("\a")
     
--- train.py
+++ train.py
@@ -3,7 +3,7 @@ import torch
 import torch.nn as nn
 from itertools import chain
 from torch.autograd import Variable
-from torch.utils.data import ConcatDataset, DataLoader
+from torch.utils.data import ConcatDataset, DataLoader, RandomSampler
 from torchvision import datasets, transforms
 from wilds.common.data_loaders import get_train_loader,get_eval_loader
 from models.hydranet import HydraNet
@@ -12,23 +12,23 @@ from utils import get_inf_iterator, save_model, get_inf_iterator, print_and_log
 from evaluate import evaluate
 from torch.utils.tensorboard import SummaryWriter
 from datetime import datetime
+import numpy as np
 
-def pseudo_label2(net, device, target_dataset, batch_size, threshold=0.9):
+def pseudo_label(net, device, target_dataset, batch_size, threshold=0.9):
     assert net.num_heads>1, 'Number of pseudo-heads must be 2 or more'
     net.eval()
+    device_loc = device#"cpu"
     target_dataloader = DataLoader(target_dataset,batch_size=batch_size, 
         shuffle=False)
-    excerpt_final = torch.IntTensor()
-    pseudo_labels_final = torch.IntTensor()
+    excerpt_final = torch.IntTensor().to(device_loc)
+    pseudo_labels_final = torch.IntTensor().to(device_loc)
     for batch_counter, (img,_,_) in enumerate(target_dataloader):
         _, p_outs = net(img.to(device)) # num_heads x batch_size x num_classes
-        p_outs = [p_out.data.cpu() for p_out in p_outs]
+        p_outs = [p_out.data.to(device_loc) for p_out in p_outs]
         _, pred_0 = torch.max(p_outs[0], 1)
-        # print(pred_0.shape)
-        mask_common = torch.ones(p_outs[0].shape[0]) # batch_size, 
+        mask_common = torch.ones(p_outs[0].shape[0]).to(device_loc) # batch_size, 
         for i in range(1,net.num_heads):
             _, pred = torch.max(p_outs[i],1)
-            # print(pred.shape)
             mask_common = mask_common * torch.eq(pred_0,pred)
         equal_idx = torch.nonzero(mask_common)
 
@@ -40,7 +40,6 @@ def pseudo_label2(net, device, target_dataset, batch_size, threshold=0.9):
         mask_conf = max_conf > threshold
         mask_final = max_conf*mask_common
         excerpt = torch.nonzero(mask_final).squeeze(-1)
-        # print(excerpt)
         _, pseudo_labels = torch.max(p_outs[0][excerpt, :], 1)
 
         excerpt_final = torch.cat([excerpt_final, excerpt+batch_counter*batch_size])
@@ -49,48 +48,94 @@ def pseudo_label2(net, device, target_dataset, batch_size, threshold=0.9):
         is_empty = True
     else:
         is_empty = False
+    excerpt_final = excerpt_final.cpu()
+    pseudo_labels_final = pseudo_labels_final.cpu()
     target_dataset_labelled = get_dummy(target_dataset, excerpt_final,
         pseudo_labels_final, need_dataset=True)
     return target_dataset_labelled, is_empty
 
+def source_train_bootstrap(net,device, train_dataset,val_dataset,batch_size,num_epochs,
+    model_dir,log_file,epoch_offset=0):
+    criterion = nn.CrossEntropyLoss()
+    optimizer_enc = torch.optim.Adam(net.enc.parameters(), lr=1e-4)
+    scheduler_enc = torch.optim.lr_scheduler.ExponentialLR(optimizer_enc, gamma=0.96)
+    optimizer_tHead = torch.optim.Adam(net.tHead.parameters(), lr=1e-3)
+    scheduler_tHead = torch.optim.lr_scheduler.ExponentialLR(optimizer_tHead, gamma=0.96)
+    optimizer_pHeads_arr = []
+    scheduler_pHeads_arr = []
+    if net.num_heads > 0:
+        optimizer_pHeads_arr = [torch.optim.Adam(pHead.parameters(), lr=net.num_heads*1e-3)
+                                for pHead in net.pHeads.heads]
+        scheduler_pHeads_arr = [torch.optim.lr_scheduler.ExponentialLR(optimizer_pHead, gamma=0.96)
+                            for optimizer_pHead in optimizer_pHeads_arr]
 
-# def pseudo_label(net, device, target_dataset, batch_size, threshold = 0.9):
-#     # need to make this online, GPU is giving errors.
-#     assert net.num_heads>1, 'Number of pseudo-heads must be 2 or more'
-#     net.eval()
-#     total_outs = None
-#     target_dataloader = DataLoader(target_dataset,batch_size=batch_size, shuffle=False)
-#     for img,_,_ in target_dataloader:
-#         _,p_outs = net(img.to(device)) #p_outs is of list of len num_heads of Tensors of size (BxC)
-#         print(f'length: {len(p_outs)}, shape: {p_outs[0].shape}')
-#         if total_outs is None:
-#             total_outs = p_outs
-#         else:
-#             for i in range(net.num_heads):
-#                 total_outs[i] = torch.cat((total_outs[i],p_outs[i]),0) # total outs is a list of length num_heads of Tensors of size (N,C)
+    # get_train_loader('standard', train_dataset, batch_size=batch_size)
+    writer = SummaryWriter()
 
-#     if net.num_heads>1:
-#         _, pred_0 = torch.max(total_outs[0],1)
-#         mask_common = torch.ones(total_outs.shape)
-#         for i in range(1,net.num_heads):
-#             _, pred = torch.max(total_outs[i],1)
-#             mask_common = mask_common * torch.eq(pred_0,pred)
-#         equal_idx = torch.nonzero(mask_common)
+    # val_loss, val_acc, val_cerr = evaluate(net,device,val_dataset,batch_size)
+    # print(val_cerr.shape)
+    # train with source samples
+    for epoch in range(epoch_offset,num_epochs):
+        sampler = RandomSampler(train_dataset, replacement=True, num_samples=len(train_dataset))
+        train_loader = DataLoader(train_dataset,batch_size=batch_size, 
+            sampler=sampler, drop_last=True)
+        net.train()
+        Loss_T = 0.0
+        Loss_P = 0.0
+        for img, label, _ in train_loader:
+            img = Variable(img.to(device))
+            label = Variable(label.to(device))
+            
+            optimizer_enc.zero_grad()
+            optimizer_tHead.zero_grad()
 
-#         max_pred, _ = torch.max(total_outs[0],1)
-#         for i in range(1,net.num_heads):
-#             pred,_ = torch.max(total_outs[i],1)
-#             max_pred = torch.max(torch.stack([max_pred, pred], 1), 1)
+
+            t_out, p_outs = net(img)
+            loss_t = criterion(t_out, label)
+            loss = loss_t 
+            Loss_T +=loss_t
+            # print(f"loss_t: {loss_t}")
+            rand_phead = int(np.random.randint(net.num_heads))
+            if net.num_heads>0:
+                optimizer_pHeads_arr[rand_phead].zero_grad()
+                loss_p = criterion(p_outs[rand_phead], label)
+                loss += loss_p
+                Loss_P += loss_p
+
+            loss.backward()
+
+            optimizer_enc.step()
+            optimizer_tHead.step()
+            if net.num_heads>0:
+                optimizer_pHeads_arr[rand_phead].step()
+                
+            # else:
+            #     print("train_T_Loss={:.7f}".format(loss_t))
+            #     print(type(Loss_T))
+        scheduler_enc.step()
+        scheduler_tHead.step()
+        for scheduler_pHead in scheduler_pHeads_arr:
+            scheduler_pHead.step()
+        if net.num_heads>0:
+            Loss_P = Loss_P/len(train_loader)
         
-#         filtered_idx = torch.nonzero(max_pred > torch.log(threshold)).squeeze()
-#         _, pseudo_labels = torch.max(total_outs[0][filtered_idx, :], 1)
-#         excerpt = equal_idx[filtered_idx]
-#         print(excerpt)
+        Loss_T = Loss_T/len(train_loader)
 
-#     target_dataset_labelled = get_dummy(target_dataset,excerpt,
-#         pseudo_labels,need_dataset=True)
-#     return target_dataset_labelled
+        save_model(net,os.path.join(model_dir,f"source_trained_epoch_{epoch+1}.pt"))
 
+        print_and_log(message="Epoch {}/{}: train_T_Loss={:.7f}, train_P_Loss={:.7f}".format(
+            epoch+1,num_epochs,Loss_T,Loss_P),
+            log_file=log_file)
+        writer.add_scalar("Loss/train", Loss_T.item(), epoch)
+        if (epoch+1)%5 ==0:
+            target_loss, target_accs, target_cerr, target_pHead_stats = evaluate(net,device,val_dataset,batch_size)
+            com_corr_high, com_corr, com_inc, com_inc_high, disag, p_cerr = target_pHead_stats
+            print_and_log(message="Tgt_Loss={:.7f}, Tgt_Acc={:.7f}, Tgt_Cal Error={:.7f}".format(
+                target_loss, target_accs[0], target_cerr),log_file=log_file)
+            print_and_log(message=f"Accuracies of heads = {target_accs[1]}",log_file=log_file)
+            print_and_log(message="com_corr_high={:.7f}, com_corr={:.7f}, com_inc={:.7f}, com_inc_high={:.7f}, disag={:.7f}, P_Cal Error={:.7f}".format(
+                com_corr_high, com_corr,com_inc,com_inc_high,disag,p_cerr),log_file=log_file)
+        
 def source_train(net,device, train_dataset,val_dataset,batch_size,num_epochs,
     model_dir,log_file,epoch_offset=0):
     criterion = nn.CrossEntropyLoss()
@@ -101,7 +146,6 @@ def source_train(net,device, train_dataset,val_dataset,batch_size,num_epochs,
     if net.num_heads > 0:
         optimizer_pHeads = torch.optim.Adam(net.pHeads.parameters(), lr=1e-3)
         scheduler_pHeads = torch.optim.lr_scheduler.ExponentialLR(optimizer_pHeads, gamma=0.96)
-
 
     train_loader = DataLoader(train_dataset,batch_size=batch_size, 
         shuffle=True, drop_last=True)
@@ -161,10 +205,11 @@ def source_train(net,device, train_dataset,val_dataset,batch_size,num_epochs,
             log_file=log_file)
         writer.add_scalar("Loss/train", Loss_T.item(), epoch)
         if(epoch+1)%5 ==0:
-            val_loss, val_acc, val_cerr, val_pHead_stats = evaluate(net,device,val_dataset,batch_size)
-            com_corr_high, com_corr, com_inc, com_inc_high, disag, p_cerr = val_pHead_stats
-            print_and_log(message="Val_Loss={:.7f}, Val_Acc={:.7f}, Val_Cal Error={:.7f}".format(
-                val_loss, val_acc,val_cerr),log_file=log_file)
+            target_loss, target_accs, target_cerr, target_pHead_stats = evaluate(net,device,val_dataset,batch_size)
+            com_corr_high, com_corr, com_inc, com_inc_high, disag, p_cerr = target_pHead_stats
+            print_and_log(message="Tgt_Loss={:.7f}, Tgt_Acc={:.7f}, Tgt_Cal Error={:.7f}".format(
+                target_loss, target_accs[0], target_cerr),log_file=log_file)
+            print_and_log(message="Accuracies of heads = {:.7f}".format(target_accs[1:]))
             print_and_log(message="com_corr_high={:.7f}, com_corr={:.7f}, com_inc={:.7f}, com_inc_high={:.7f}, disag={:.7f}, P_Cal Error={:.7f}".format(
                 com_corr_high, com_corr,com_inc,com_inc_high,disag,p_cerr),log_file=log_file)
                 
@@ -174,7 +219,7 @@ def domain_adapt(net, device, source_dataset, target_dataset,
         return
 
     # get pseudolabels
-    target_dataset_labelled, is_empty = pseudo_label2(net, device, target_dataset, batch_size, threshold)
+    target_dataset_labelled, is_empty = pseudo_label(net, device, target_dataset, batch_size, threshold)
     if is_empty:
         print("No pseudo labelled points")
         return 
@@ -242,58 +287,3 @@ def domain_adapt(net, device, source_dataset, target_dataset,
         print_and_log(message="Domain Adapt Step {} Epoch {}/{}: Target Loss={:.7f}, Pseudo Loss={:.7f}".format(
             k_step, epoch+1,num_adapt_epochs,Loss_T, Loss_P),
             log_file=log_file)
-
-# def domain_adapt(net, device, source_dataset, target_dataset, batch_size, num_pseudo_steps, num_adapt_epochs, n_t):
-#     # domain adaptation
-#     if net.num_heads == 0:
-#         return
-
-#     # get pseudolabels
-#     target_dataset_labelled = pseudo_label(net,target_dataset,n_t)
-#     merged_dataset = ConcatDataset([source_dataset, target_dataset_labelled])
-
-#     criterion = nn.CrossEntropyLoss()
-#     optimizer_pseudo = torch.optim.Adam(list(net.enc.parameters())+list(net.pHeads.parameters()),
-#         lr=1e-4, weight_decay=0.96)
-#     optimizer_target = torch.optim.Adam(list(net.enc.parameters())+list(net.tHead.parameters()),
-#         lr=1e-4, weight_decay=0.96)
-
-#     for k in range(num_pseudo_steps):
-#         net.train()
-#         merged_dataloader = DataLoader(merged_dataset,batch_size=batch_size, shuffle=True)
-#         target_dataloader_labelled = get_inf_iterator(DataLoader(target_dataset_labelled,batch_size=batch_size, shuffle=True))
-
-#         for adapt_step in range(num_adapt_epochs):
-#             for i_tt, (img, label, _) in enumerate(merged_dataloader):
-#                 pseudo_img, pseudo_target_labels = next(target_dataloader_labelled)
-#                 img = Variable(img.to(device))
-#                 label = Variable(label.to(device))
-#                 pseudo_img = Variable(pseudo_img)
-#                 pseudo_target_labels = Variable(pseudo_target_labels)
-
-#                 optimizer_pseudo.zero_grad()
-#                 optimizer_target.zero_grad()
-#                 # Train F, F_heads with merged dataset
-#                 t_out, p_outs = net(img)
-#                 loss_heads = []
-#                 for p_out in p_outs:
-#                     loss_p = criterion(p_out, label)
-#                     loss_p.backward()
-#                     loss_heads.append(loss_p)
-#                 optimizer_pseudo.step()
-                
-#                 # Train F, F_t with pseudo-labelled target data
-#                 t_out, _ = net(pseudo_img)
-#                 loss_t = criterion(t_out, pseudo_target_labels)
-#                 loss_t.backward()
-#                 optimizer_target.step()
-
-#             n_t = adapt_step*len(source_dataset)//20
-#             target_dataset_labelled = pseudo_label(net,target_dataset,n_t)
-#             merged_dataset = ConcatDataset([source_dataset, target_dataset_labelled])
-
-# labeling function
-# evaluate the heads
-
-#VALIDDATION Compute CALIBRATION
-
