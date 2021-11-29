9db8528854f5aca88de245507e382bc7eec83ddd
Anmol Kagrecha <akagrecha@gmail.com>
2021-11-28 18:21:45 -0800

    fixed main.py

--- main.py
+++ main.py
@@ -19,7 +19,9 @@ def main():
     parser.add_argument('--num_classes', type=int, default=62)    
     parser.add_argument('--orig_frac', type=float, default=1.0, help="fraction of data to be used while training, useful to set to 5e-2 for local runs")
     parser.add_argument('--threshold', type=float, default=0.9)  
-    parser.add_argument('--bootstrap', type=bool, default=False)   
+    parser.add_argument('--bootstrap', type=bool, default=False)  
+    parser.add_argument('--saved_model_path', default=None)  
+    parser.add_argument('--da_epoch_offset', type=int, required=False, default=0)
 
     args = parser.parse_args()
     target_domain = args.target_domain
@@ -32,6 +34,8 @@ def main():
     num_classes = args.num_classes
     orig_frac = args.orig_frac 
     threshold = args.threshold
+    saved_model_path = args.saved_model_path
+    da_epoch_offset = args.da_epoch_offset
 
     print("Num heads=",num_pseudo_heads)
     if num_pseudo_heads>0:
@@ -54,7 +58,8 @@ def main():
     net = HydraNet(num_heads=num_pseudo_heads, num_features=1024,
         num_classes=num_classes,pretrained=True)
     net = net.to(device)
-    # net.load_state_dict(torch.load("model_weights/num_heads_2/2021-11-26-20-41-45/source_trained_epoch_30.pt"))
+    if saved_model_path is not None:
+        net.load_state_dict(torch.load(saved_model_path))
 
     if args.bootstrap:
         print("SOURCE TRAINING WITH BOOTSTRAPPING")
@@ -64,7 +69,7 @@ def main():
         source_train(net, device, train_dataset, target_dataset, batch_size,num_epochs,model_dir,log_file,epoch_offset)
     
     if num_pseudo_heads>0:
-        for k in range(1,num_pseudo_steps+1):
+        for k in range(1+da_epoch_offset,num_pseudo_steps+1):
             frac = min(1., k/num_pseudo_steps)
             target_dataset_frac = dataset.get_subset(target_domain, frac=orig_frac*frac,
         transform=transforms.Compose([transforms.Resize((224,224)),transforms.ToTensor()]))
@@ -72,12 +77,21 @@ def main():
                 batch_size, k, num_adapt_epochs, threshold, model_dir,log_file)
 
             if (k%5) ==0:
-                tatget_loss, target_acc, target_cerr, target_pHead_stats = evaluate(net,device,target_dataset,batch_size)
+                target_loss, target_accs, target_cerr, target_pHead_stats = evaluate(net,device,target_dataset,batch_size)
                 com_corr_high, com_corr, com_inc, com_inc_high, disag, p_cerr = target_pHead_stats
-                print_and_log(message="Target_Loss={:.7f}, Target_Acc={:.7f}, Target_Cal Error={:.7f}".format(
-                    tatget_loss, target_acc,target_cerr),log_file=log_file)
+                print_and_log(message="Tgt_Loss={:.7f}, Tgt_Acc={:.7f}, Tgt_Cal Error={:.7f}".format(
+                    target_loss, target_accs[0], target_cerr),log_file=log_file)
+                print_and_log(message="Accuracies of heads = {:.7f}".format(target_accs[1:]))
                 print_and_log(message="com_corr_high={:.7f}, com_corr={:.7f}, com_inc={:.7f}, com_inc_high={:.7f}, disag={:.7f}, P_Cal Error={:.7f}".format(
                     com_corr_high, com_corr,com_inc,com_inc_high,disag,p_cerr),log_file=log_file)
+                
+
+                # tatget_loss, target_acc, target_cerr, target_pHead_stats = evaluate(net,device,target_dataset,batch_size)
+                # com_corr_high, com_corr, com_inc, com_inc_high, disag, p_cerr = target_pHead_stats
+                # print_and_log(message="Target_Loss={:.7f}, Target_Acc={:.7f}, Target_Cal Error={:.7f}".format(
+                #     tatget_loss, target_acc,target_cerr),log_file=log_file)
+                # print_and_log(message="com_corr_high={:.7f}, com_corr={:.7f}, com_inc={:.7f}, com_inc_high={:.7f}, disag={:.7f}, P_Cal Error={:.7f}".format(
+                #     com_corr_high, com_corr,com_inc,com_inc_high,disag,p_cerr),log_file=log_file)
     log_file.close()
 
 if __name__=="__main__":
